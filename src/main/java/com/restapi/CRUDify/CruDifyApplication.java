193537583354bd4bf319141df7299539d828964f
shruti832 <sc947105@gmail.com>
2024-11-21 17:29:01 +0530

    Project Setup

---
+++ src/main/java/com/restapi/CRUDify/CruDifyApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.CRUDify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/CRUDify/CruDifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.CRUDify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
